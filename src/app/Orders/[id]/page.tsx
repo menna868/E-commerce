7e77f6f9e54fe486e374fa03078ad0d067291211
menna868 <menna.moustafa868@gmail.com>
2025-09-19 18:06:08 +0300

    Update page.tsx

--- src/app/Orders/[id]/page.tsx
+++ src/app/Orders/[id]/page.tsx
@@ -4,7 +4,7 @@ import OrderDetails from '@/app/_components/orderDetails/OrderDetails';
 import React from 'react'
 
 export default async function ProductDetails({ params }:{params:Promise<{id:string}>}) {
-  let { id } = params;
+  let { id } =await params;
   let data = await UserOrdersApi(id);
   if (!data) return <h1>No data here</h1>;
 
