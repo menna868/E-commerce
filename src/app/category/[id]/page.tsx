8035b70bf9ba00f070e7e7439dd78272bf973c8e
menna868 <menna.moustafa868@gmail.com>
2025-09-19 17:31:30 +0300

    Update page.tsx

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -2,9 +2,8 @@ import SelectedCategory from "@/Api/selectedCtegory";
 import CategoryProductDetails from "@/app/_components/categorydetails/CategoryDetails";
 import React from "react";
 
-export default async function categoryDetails({params}: {params: { id: string };
-}) {
-  const { id } = params;
+export default async function categoryDetails({ params }:{params:Promise<{id:string}>}) {
+  let{id} =await params;
   const data = await SelectedCategory(id);
   return <CategoryProductDetails data={data} />;
 }
